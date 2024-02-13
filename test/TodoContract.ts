3bad563269b549fdbbf6690e7f0a7f163f46fa58
Jeremiah Samuel <livinalt@gmail.com>
2024-02-13 10:26:11 +0100

    testing TodoContract

--- test/TodoContract.ts
+++ test/TodoContract.ts
@@ -1,39 +1,37 @@
 import {
-  time,
   loadFixture,
 } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Lock", function () {
+describe("TodoContract", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployTodoContractFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
+  async function deployTodoContract() {
+    
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const TodoContract = await ethers.getContractFactory("TodoContract");
+    const todoContract = await TodoContract.deploy();
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    return { todoContract};
   }
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+  describe("Create Todo", function () {
+    it("Should create a todo instance", async function () {
+      const { todoContract } = await loadFixture(deployTodoContract);
+      const create = await todoContract.createTodo("Jerry", "Staff Meeting","started");
+      const todoList = await todoContract.getAllList() 
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+      expect(todoList.length).to.equal(1);
+      expect(todoList[0].title).to.equal("Jerry");
+      expect(todoList[0].description).to.equal("Staff Meeting");
+      expect(todoList[0].status).to.equal("started");
     });
 
-    it("Should set the right owner", async function () {
+    /* it("Should set the right owner", async function () {
       const { lock, owner } = await loadFixture(deployOneYearLockFixture);
 
       expect(await lock.owner()).to.equal(owner.address);
@@ -56,10 +54,11 @@ describe("Lock", function () {
       await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
         "Unlock time should be in the future"
       );
-    });
+    }); 
   });
+  */
 
-  describe("Withdrawals", function () {
+/*   describe("Withdrawals", function () {
     describe("Validations", function () {
       it("Should revert with the right error if called too soon", async function () {
         const { lock } = await loadFixture(deployOneYearLockFixture);
@@ -122,6 +121,7 @@ describe("Lock", function () {
           [lockedAmount, -lockedAmount]
         );
       });
-    });
+    }); */
+
   });
 });
