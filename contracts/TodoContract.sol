b3f5bb23be6c4ed08d73b0294395cd65253e7d94
Jeremiah Samuel <livinalt@gmail.com>
2024-02-13 08:10:13 +0100

    function to  created

--- contracts/TodoContract.sol
+++ contracts/TodoContract.sol
@@ -12,15 +12,25 @@ contract TodoContract{
     // creating an enum to hold the different status of the to-do list
     enum Status{done, pending, started}
 
+    // mapping the various status to the bool status of the struct
+    mapping(Status => Todo.status) mappedStatus;
+
     // creating an array to hold the to-do lists
     Todo[] todoLists;
 
-    function createTodo() public {
-
+    function createTodo(string memory _title, string memory _desc, string memory _status) public {
+        uint256 todoId = todoLists.length;
+        Todo memory newList = Todo(todoId, _title, _desc, _status);        
+        todoLists.push(newList);
+        
     }
 
-    function updateTodoList() public {
-
+    function updateTodoList(uint256 _id, string memory _title, string memory _desc, string memory _status) public returns(string memory, string memory, string memory){
+        require(_id < todoLists.length, "");
+        todoLists[_id].title = _title;
+        todoLists[_id].description = _desc;
+        todoLists[_id].status = _status;
+        // return (todoId, Todo.title, Todo.description, Todo.status);
     }
 
     function updateTodoStatus() public{
