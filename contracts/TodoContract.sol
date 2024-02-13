154c3bc8ce891a366d90da0bfb210b63d910ee2b
Jeremiah Samuel <livinalt@gmail.com>
2024-02-13 09:26:08 +0100

    contract completed

--- contracts/TodoContract.sol
+++ contracts/TodoContract.sol
@@ -5,18 +5,24 @@ contract TodoContract{
 
     // createing a struct to hold the various data types present in the todo
     struct Todo{
+        uint256 Id;
         string title;
         string description;
-        bool status;
+        string status;
     }
-    // creating an enum to hold the different status of the to-do list
-    enum Status{done, pending, started}
+    // create an enum to hold the different status of the to-do list
+    enum Status{done, pending, started, created}
 
     // mapping the various status to the bool status of the struct
-    mapping(Status => Todo) mappedStatus;
+    mapping(Status => Todo[]) mappedStatus;
+    // mapping(address => msg.sender) owner;
 
     // creating an array to hold the to-do lists
-       Todo[] todoLists;
+    Todo[] todoLists;
+
+    constructor() {
+        // owner[msg.sender] = msg.sender;
+    }
 
     function createTodo(string memory _title, string memory _desc, string memory _status) public {
         uint256 todoId = todoLists.length;
@@ -35,15 +41,29 @@ contract TodoContract{
 
     function updateStatus(uint256 _id, string memory _status) public returns(Todo memory){
         //to update the status of a todo, 
-        // select the todo by its unique identifier
+        // select the todo by its unique identifier id
         require(_id < todoLists.length, "");
         // require(_status == mappedStatus, "");
             todoLists[_id].status = _status;
             return todoLists[_id];      
     }
 
-    function deleteTodoList() public {
+    function deleteTodoList(uint256 _id) public {
+        require(_id<todoLists.length, "");
+        return todoLists.pop();
 
+    }
+
+    function retrieveTodo(uint _id) public view returns(Todo memory){
+        return todoLists[_id];
+    }
+    
+    function getAllList() public view returns(Todo[] memory){
+        return todoLists;
+    }
+
+    function getStatus(uint256 _id) public view returns(string memory){
+        return todoLists[_id].status;
     }
 
 }
